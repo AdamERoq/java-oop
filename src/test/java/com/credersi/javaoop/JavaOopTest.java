06020b1df2a22d2ccc63f3523e49a7b1e5d29b48
Adam Eckley <adam.eckley@roq.co.uk>
2023-02-28 15:26:32 +0000

    Tests developed for the Racecar class and sub-classes

--- src/test/java/com/credersi/javaoop/JavaOopTest.java
+++ src/test/java/com/credersi/javaoop/JavaOopTest.java
@@ -1,12 +1,87 @@
 package com.credersi.javaoop;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class JavaOopTest {
 	@Test
-	public void testsToBeDefined() {
-		assertTrue(true);
-	}
+    public void testRacing() {
+    	McGredez mcGredez = new McGredez();
+    	assertTrue(mcGredez.isRacing());
+    	
+    }
+    
+    @Test
+    public void testtyrewear() {
+    	McGredez mcGredez = new McGredez();
+    	mcGredez.wear();
+    	assertTrue(mcGredez.isRacing());
+    	
+    }
+    
+    @Test
+    public void testtyrewearfail() {
+    	McGredez mcGredez = new McGredez();
+    	mcGredez.wear();
+    	mcGredez.wear();
+    	assertFalse(mcGredez.isRacing());
+    	
+    }
+    
+    @Test
+    public void testRefuel() {
+    	McGredez mcGredez = new McGredez();
+    	mcGredez.refuel();
+    	assertTrue(mcGredez.isRacing());
+    	
+    }
+    
+    @Test
+    public void testRefuelReset() {
+    	McGredez mcGredez = new McGredez();
+    	mcGredez.reset();
+    	mcGredez.refuel();
+    	assertTrue(mcGredez.isRacing());	
+    }
+    
+    @Test
+    public void testRefuelResetToBreakdown() {
+    	McGredez mcGredez = new McGredez();
+    	mcGredez.reset();
+    	mcGredez.refuel();
+    	mcGredez.refuel();
+    	mcGredez.refuel();
+    	assertFalse(mcGredez.isRacing());	
+    }
+    
+    @Test
+    public void testReset() {
+    	McGredez mcGredez = new McGredez();
+    	mcGredez.reset();
+    	assertTrue(mcGredez.isRacing());	
+    }
+    
+    @Test
+    public void testBreakdown() {
+    	McGredez mcGredez = new McGredez();
+    	mcGredez.breakdown();
+    	assertFalse(mcGredez.isRacing());	
+    }
+    
+    @Test
+    public void testRaces() {
+    	McGredez mcGredez = new McGredez();
+    	mcGredez.races();
+    	assertTrue(mcGredez.isRacing());	
+    }
+    
+    @Test
+    public void testRacesReset() {
+    	McGredez mcGredez = new McGredez();
+    	mcGredez.reset();
+    	mcGredez.races();
+    	assertTrue(mcGredez.isRacing());	
+    }
+    
 }
